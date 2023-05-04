7580a3ac3722dd03c6110db8cb2e8dfb2e45a516
exam <exam@Bcalab4-m19>
2023-05-04 13:01:44 +0530

    commit

---
+++ PiggyBank_244/src/PiggyBank_244/PiggyBank_244.java
@@ -0,0 +1,17 @@
+package PiggyBank_244;
+
+public class PiggyBank_244 {
+	int num_coins;
+	int coin_value;
+	
+	PiggyBank_244(int num_coins)
+	{
+		this.num_coins = num_coins;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PiggyBank_244/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author exam
+ *
+ */
+module PiggyBank_244 {
+}
